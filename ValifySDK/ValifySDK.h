482138a8da9f6275cde4e8d06fc15ca07cb27f62
Peter <peter.samir@fawry.com>
2024-08-29 16:16:04 +0300

    Initial Commit

---
+++ ValifySDK/ValifySDK.h
@@ -0,0 +1,18 @@
+//
+//  ValifySDK.h
+//  ValifySDK
+//
+//  Created by Peter Samir on 29/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValifySDK.
+FOUNDATION_EXPORT double ValifySDKVersionNumber;
+
+//! Project version string for ValifySDK.
+FOUNDATION_EXPORT const unsigned char ValifySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValifySDK/PublicHeader.h>
+
+
